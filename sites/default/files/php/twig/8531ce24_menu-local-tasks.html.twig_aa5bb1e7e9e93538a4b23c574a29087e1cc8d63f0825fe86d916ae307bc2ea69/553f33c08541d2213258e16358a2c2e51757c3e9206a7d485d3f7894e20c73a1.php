b38100fddc50dc025ac47cb134d6f4026e5f9db3
Ivanich <gorbukov@gmail.com>
2016-11-22 21:51:53 +0300

    first commit

---
+++ sites/default/files/php/twig/8531ce24_menu-local-tasks.html.twig_aa5bb1e7e9e93538a4b23c574a29087e1cc8d63f0825fe86d916ae307bc2ea69/553f33c08541d2213258e16358a2c2e51757c3e9206a7d485d3f7894e20c73a1.php
@@ -0,0 +1,123 @@
+<?php
+
+/* themes/basic/templates/menu-local-tasks.html.twig */
+class __TwigTemplate_d07cab2abf1af57b9ab0f57b1e30843001bdc8d6f8e7d325d3ea59de46a0ddba extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 21);
+        $filters = array("t" => 22);
+        $functions = array("attach_library" => 19);
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('if'),
+                array('t'),
+                array('attach_library')
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 18
+        echo "
+";
+        // line 19
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->env->getExtension('drupal_core')->attachLibrary("basic/tabs"), "html", null, true));
+        echo "
+
+";
+        // line 21
+        if ((isset($context["primary"]) ? $context["primary"] : null)) {
+            // line 22
+            echo "  <h2 class=\"visually-hidden\">";
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->renderVar(t("Primary tabs")));
+            echo "</h2>
+  <ul class=\"tabs primary\">";
+            // line 23
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["primary"]) ? $context["primary"] : null), "html", null, true));
+            echo "</ul>
+";
+        }
+        // line 25
+        if ((isset($context["secondary"]) ? $context["secondary"] : null)) {
+            // line 26
+            echo "  <h2 class=\"visually-hidden\">";
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->renderVar(t("Secondary tabs")));
+            echo "</h2>
+  <ul class=\"tabs secondary\">";
+            // line 27
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["secondary"]) ? $context["secondary"] : null), "html", null, true));
+            echo "</ul>
+";
+        }
+        // line 29
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/basic/templates/menu-local-tasks.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  75 => 29,  70 => 27,  65 => 26,  63 => 25,  58 => 23,  53 => 22,  51 => 21,  46 => 19,  43 => 18,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Default theme implementation to display primary and secondary local tasks.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - primary: HTML list items representing primary tasks.*/
+/*  * - secondary: HTML list items representing primary tasks.*/
+/*  **/
+/*  * Each item in these variables (primary and secondary) can be individually*/
+/*  * themed in menu-local-task.html.twig.*/
+/*  **/
+/*  * @see template_preprocess_menu_local_tasks()*/
+/*  **/
+/*  * @ingroup themeable*/
+/*  *//* */
+/* #}*/
+/* */
+/* {{ attach_library('basic/tabs') }}*/
+/* */
+/* {% if primary %}*/
+/*   <h2 class="visually-hidden">{{ 'Primary tabs'|t }}</h2>*/
+/*   <ul class="tabs primary">{{ primary }}</ul>*/
+/* {% endif %}*/
+/* {% if secondary %}*/
+/*   <h2 class="visually-hidden">{{ 'Secondary tabs'|t }}</h2>*/
+/*   <ul class="tabs secondary">{{ secondary }}</ul>*/
+/* {% endif %}*/
+/* */
+/* */
